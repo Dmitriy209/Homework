621ef6a297592277d54f9ead28a410b64432845c
Dmitriy209 <dim286754@gmail.com>
2024-05-28 21:53:02 +0500

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisInt;
+            float thisFloat;
+            char thisChar;
+            string thisString;
+            bool thisBool;
+            double thisDouble;
+            uint thisUInt;
+            short thisShort;
+            ushort thisUShort;
+            long thisLong;
+        }
+    }
+}
