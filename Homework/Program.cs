15c9c761f91683be9a0fc0c58549afd8c64659ea
Dmitriy209 <dim286754@gmail.com>
2024-06-12 12:06:01 +0500

    Ver1.0

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -10,16 +10,16 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            int thisInt;
-            float thisFloat;
-            char thisChar;
-            string thisString;
-            bool thisBool;
-            double thisDouble;
-            uint thisUInt;
-            short thisShort;
-            ushort thisUShort;
-            long thisLong;
+            int thisInt = -33000;
+            float thisFloat = 6.8f;
+            char thisChar = 'C';
+            string thisString = "Строка";
+            bool thisTrue = true;
+            double thisDouble = 6.8;
+            uint thisUInt = 33000;
+            short thisShort = -32000;
+            ushort thisUShort = 32000;
+            long thisMaxValue = long.MaxValue;
         }
     }
 }
