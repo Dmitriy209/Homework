47eb65705e9f2ba5dcdcbe696d45cedc7df0a8f8
Dmitriy209 <80581870+Dmitriy209@users.noreply.github.com>
2024-06-27 12:34:45 +0500

    ReadInt

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,125 +1,39 @@
 using System;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace BracketExpression
+namespace Readlnt
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
+            int userNumber = GetUserNumber();
 
-            char[,] map = ReadMap("map.txt");
-            ConsoleKeyInfo pressedKey = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
-
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    pressedKey = Console.ReadKey();
-                }
-            });
-
-            int pacmanX = 1;
-            int pacmanY = 1;
-            int score = 0;
-
-            while (true)
-            {
-                Console.Clear();
-
-                HandleInput(pressedKey, ref pacmanX, ref pacmanY, map, ref score);
-
-                Console.ForegroundColor = ConsoleColor.Blue;
-                DrawMap(map);
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.SetCursorPosition(pacmanX, pacmanY);
-                Console.Write("@");
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.SetCursorPosition(19, 0);
-                Console.Write($"Score: {score}");
-
-                Thread.Sleep(1000);
-            }
+            Console.WriteLine($"Вы ввели {userNumber}.");
         }
 
-        private static char[,] ReadMap(string path)
+        private static int GetUserNumber()
         {
-            string[] file = File.ReadAllLines("map.txt");
+            int userNumber = 0;
 
-            char[,] map = new char[GetMaxLengthOfLine(file), file.Length];
+            bool isRunning = true;
 
-            for (int x = 0; x < map.GetLength(0); x++)
-                for (int y = 0; y < map.GetLength(1); y++)
-                    map[x, y] = file[y][x];
-
-            return map;
-        }
-
-        private static void DrawMap(char[,] map)
-        {
-            for (int y = 0; y < map.GetLength(1); y++)
+            while (isRunning)
             {
-                for (int x = 0; x < map.GetLength(0); x++)
+                Console.WriteLine("Введите целочисленное число:");
+                string inputLine = Console.ReadLine();
+
+                bool isUserNumber = int.TryParse(inputLine, out userNumber);
+
+                if (isUserNumber)
+                    isRunning = false;
+                else
                 {
-                    Console.Write(map[x, y]);
-                }
-
-                Console.Write("\n");
-            }
-        }
-
-        private static void HandleInput(ConsoleKeyInfo pressedKey, ref int pacmanX, ref int pacmanY, char[,] map, ref int score)
-        {
-            int[] direction = GetDirection(pressedKey);
-
-            int nextPacmanPositionX = pacmanX + direction[0];
-            int nextPacmanPositionY = pacmanY + direction[1];
-
-            char nextCell = map[nextPacmanPositionX, nextPacmanPositionY];
-
-            if (nextCell == ' ' || nextCell == '.')
-            {
-                pacmanX = nextPacmanPositionX;
-                pacmanY = nextPacmanPositionY;
-
-                if (nextCell == '.')
-                {
-                    score++;
-                    map[nextPacmanPositionX, nextPacmanPositionY] = ' ';
+                    Console.Clear();
+                    Console.WriteLine($"{inputLine} - не целочисленное число.");
                 }
             }
-        }
 
-        private static int[] GetDirection(ConsoleKeyInfo pressedKey)
-        {
-            int[] direction = { 0, 0 };
-
-            if (pressedKey.Key == ConsoleKey.UpArrow)
-                direction[1] = -1;
-            else if (pressedKey.Key == ConsoleKey.DownArrow)
-                direction[1] = 1;
-            else if (pressedKey.Key == ConsoleKey.LeftArrow)
-                direction[0] = -1;
-            else if (pressedKey.Key == ConsoleKey.RightArrow)
-                direction[0] = 1;
-
-            return direction;
-        }
-
-        private static int GetMaxLengthOfLine(string[] lines)
-        {
-            int maxLength = lines[0].Length;
-
-            foreach (var line in lines)
-                if (line.Length > maxLength)
-                    maxLength = line.Length;
-
-            return maxLength;
+            return userNumber;
         }
     }
 }
